3e59b852bc838428fe3004d5c565996985adb97d
Roberto Kennedy <roberto.kpp@gmail.com>
2026-02-25 22:59:50 -0500

    feat: add Prisma setup with PostgreSQL, environment configuration, and initial migrations

---
+++ prisma/migrations/20260226035541_/migration.sql
@@ -0,0 +1,47 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('customer', 'sale');
+
+-- CreateEnum
+CREATE TYPE "DeliveryStatus" AS ENUM ('processing', 'shipped', 'delivered');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'customer',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "deliveries" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "DeliveryStatus" NOT NULL DEFAULT 'processing',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "deliveries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "delivery_log" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "delivery_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "delivery_log_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "delivery_log" ADD CONSTRAINT "delivery_log_delivery_id_fkey" FOREIGN KEY ("delivery_id") REFERENCES "deliveries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
