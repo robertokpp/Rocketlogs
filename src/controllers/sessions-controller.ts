17896ed35058062d41fe742c6d4de0aa3064d7a7
Roberto Kennedy <roberto.kpp@gmail.com>
2026-02-27 22:35:27 -0500

    feat: implement user authentication with JWT and session management

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,16 +1,43 @@
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 import { z } from "zod";
+import { compare } from "bcrypt";
 
 class SessionsController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     const bodySchema = z.object({
       email: z.string().email(),
-      password: z.string().min(6),
+      password: z.string(),
     });
 
     const { email, password } = bodySchema.parse(request.body);
 
-    return response.json({ massage: "ok" });
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role ?? "customer" }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const { password: hashedPassword, ...userWithoutPassword } = user;
+    return response.json({ token, ...userWithoutPassword });
   }
 }
 
